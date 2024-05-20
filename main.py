2b3e26ddb22b313e5e15fe5af588f0bc76352844
Borunov Semyon <lol1488trol@gmail.com>
2024-05-20 17:13:04 +0300

    minimal weather_bot + city db

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 
 with open("TOKEN") as f:
     bot = telebot.TeleBot(f.read())
-api_token = 'e898af2abc276415b2b67e613ba02639'
+with open("APITOKEN") as f:
+    api_token = f.read()
 my_city = {680890776: "Москва"}
 
 
@@ -14,8 +15,11 @@ def make_txt_pretty(text):
 
 def get_weather_by_place(place):
     link = f'http://api.openweathermap.org/data/2.5/weather?q={place}&lang=ru&units=metric&appid={api_token}'
-    responce = requests.get(link).json()
+    responce = requests.get(link)
+    if responce.status_code != 200:
+        return "кажется такого города нет"
     print(responce)
+    responce = responce.json()
     temp_min = responce["main"]["temp_min"]
     temp_max = responce["main"]["temp_max"]
 
@@ -23,8 +27,8 @@ def get_weather_by_place(place):
     wind = responce["wind"]["speed"]
 
     description = responce['weather'][0]['description']
-    return f'Погода в городе {place}:\n{description} \nТемпература от {temp_min}°C до {temp_max}°C\nДавление {pressure} мм рт ст\n' \
-           f'Скорость ветра {wind} м/c\n\n'
+    return f'Погода в городе {place.capitalize()}:\n{description.capitalize()} \nТемпература от {temp_min}°C' \
+           f' до {temp_max}°C\nДавление {pressure} мм рт ст\nСкорость ветра {wind} м/c'
 
 
 @bot.message_handler(content_types=['text'])
@@ -32,23 +36,22 @@ def get_text_messages(message):
     txt = make_txt_pretty(message.text)
 
     if txt == "привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+        bot.send_message(message.from_user.id,
+                         "Привет, чтобы узнать что я могу, напиши '/help', чтобы познакомиться, напиши '/start'")
     elif txt == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
+        bot.send_message(message.from_user.id, "чета надо умное написать")
     elif txt == 'погода':
         bot.send_message(message.from_user.id, get_weather_by_place(my_city[message.from_user.id]))
+    elif txt == "/start":
+        bot.send_message(message.from_user.id, 'Привет, я бот, подсказывающий погоду, где ты обитаешь?')
+        bot.register_next_step_handler(message, memorize_place)
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-
-@bot.message_handler(content_types=["start"])
-def start(message):
-    bot.send_message(message.from_user.id, 'Привет, я бот, подсказывающий погоду, где ты обитаешь?')
-    bot.register_next_step_handler(message, memorize_place)
+        bot.send_message(message.from_user.id, get_weather_by_place(txt))
 
 
 def memorize_place(message):
-    pass
+    my_city[message.from_user.id] = make_txt_pretty(message.text)
+    bot.send_message(message.from_user.id, "Отлично, я запомнил")
 
 
 bot.polling(none_stop=True, interval=0)
